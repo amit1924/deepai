8c401dab645b92b84539969ab0c099a97c83a131
Amit Kumar <amit192400@gmail.com>
2025-09-24 14:36:45 +0530

    modify utils.js in backend

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,9 +11,13 @@ export const generateToken = (userId, res) => {
   });
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: NODE_ENV === 'development' ? false : true,
-    sameSite: 'lax', // use "lax" instead of "strict" for REST Client
-    // sameSite: 'strict',
+    // secure: NODE_ENV === 'development' ? false : true,
+    // sameSite: 'lax', // use "lax" instead of "strict" for REST Client
+    // // sameSite: 'strict',
+
+    sameSite: NODE_ENV === 'development' ? 'lax' : 'none', // Lax for localhost, None for production
+    secure: NODE_ENV === 'development' ? false : true, // HTTPS required in production
+
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
   return token;
