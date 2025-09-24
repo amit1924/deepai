17ae55a8be7631768d64cffa167d89222062be9d
Amit Kumar <amit192400@gmail.com>
2025-09-24 14:20:29 +0530

    added env in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,16 +1,21 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'http://localhost:3000/api'
-      : 'https://deepai-backend.onrender.com/api',
-  withCredentials: true,
-});
-
 // import axios from 'axios';
 
 // export const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   withCredentials: true, // to send cookies
+//   baseURL:
+//     import.meta.env.MODE === 'development'
+//       ? 'http://localhost:3000/api'
+//       : '/api',
+//   withCredentials: true,
 // });
+
+import axios from 'axios';
+
+const baseURL =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:3000/api'
+    : import.meta.env.VITE_API_BASE_URL;
+
+export const axiosInstance = axios.create({
+  baseURL: baseURL,
+  withCredentials: true,
+});
