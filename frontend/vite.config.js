f469b004ef60117f93ea694c883fd46421100b7b
Amit Kumar <amit192400@gmail.com>
2025-09-26 13:25:57 +0530

    added gemini ai config in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@google/generative-ai'],
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
 });
